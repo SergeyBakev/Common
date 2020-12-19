74fa7223066a17873d45a588bbe1c0f02c23e64a
Бакаев Сергей <daspreichp88@gmail.com>
2020-12-19 07:33:11 +0300

    Add tests project for Common lib

---
+++ Common/CommonTests/CommonTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CommonTests
+{
+	TEST_CLASS(CommonTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
